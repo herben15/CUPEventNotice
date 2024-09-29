c4c1874f3e8ca885dcf073724a221f12c772da45
herben <134608526+herben15@users.noreply.github.com>
2024-09-29 17:19:20 +0800

    Update 修改测试程序

--- src/test.py
+++ src/test.py
@@ -4,9 +4,18 @@ import sys
 import fecth
 import schedule
 import time
+import get_cookies
 
 if __name__=='__main__':
     fecth.init()
+    print("测试cookie是否获取正常")
+    P=get_cookies.get_cookies()
+    if P:
+        print(f"获取成功，其值为：{P}")
+    else:
+        print("获取失败，请检查程序")
+        sys.exit()
+    fecth.cookies['PHPSESSID'] = P
     print("测试是否正常获取数据")
     if fecth.fetch_activities():
         print("获取数据正常，请检查是否获取到邮件")
@@ -37,15 +46,3 @@ if __name__=='__main__':
     else:
         print("程序异常，请修改程序")
         sys.exit()
-
-    print("测试异常退出时能否及时通知，请查看邮箱")
-    cnt = 1
-    limit = 15
-    schedule.every(5).seconds.do(fecth.job)
-    while cnt <= limit:
-        schedule.run_pending()
-        time.sleep(1)
-        cnt+=1
-        print("检查...")
-    fecth.log("正常退出")
-    print("异常！job函数异常，没有异常退出")
